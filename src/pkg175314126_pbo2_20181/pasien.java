4fd597b5d267e57849837be1fbd0abad3de47c3c
admin <admin@17b>
2018-08-29 10:35:15 +0700

    versi 0.1

---
+++ src/pkg175314126_pbo2_20181/pasien.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg175314126_pbo2_20181;
+
+/**
+ *
+ * @author admin
+ */
+public class pasien {
+    private String nama;
+    
+  
+}
